46a991fc82f097093fb03d696947d823b5eec6f5
Youness Ait Moh <youness.aitmoh.ensaj@gmail.com>
2024-10-29 15:30:32 +0100

    User repo

---
+++ src/main/java/com/ensa/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ensa.demo.repositories;
+
+import com.ensa.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+}
